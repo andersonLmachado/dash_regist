c68e7b1aeb4914e1b6303eed4419fcd02680c78f
Anderson Machado <88006912+andersonLmachado@users.noreply.github.com>
2024-01-02 15:59:39 -0300

    Add files via upload

---
+++ graph.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
+
+data = pd.read_excel('contagem_atendimentos.xlsx')
+st.write(data)
+
+chart_data = pd.DataFrame(
+    np.random.randn(20, 8),
+    columns=['Adami', 'Suzano', 'Klabin', 'Irani', 'RFR Guarulhos', 'RFR Indaiatuba', 'GKN', 'Coopercarga']
+)
+
+st.bar_chart(chart_data)
+st.line_chart(chart_data)
